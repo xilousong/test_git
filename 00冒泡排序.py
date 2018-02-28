b8826318922834f527baad7ee7e9724c410bff7d
xilousong <xilou19@163.com>
2018-02-28 23:27:17 +0800

    change file 00冒泡排序.py

--- 00冒泡排序.py
+++ 00冒泡排序.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-arr = [45,67,23,122,65,89,44,66,99,12,5,90,4,13,19,2,6,84,47]
+arr = [45,67,122,65,89,44,66,12,5,90,4,13,19,2,6,84,47]
 
 #计算出列表元素个数
 arr_lenth = len(arr)
-
+count = 0
 #列表中有多少个元素就循多少次
 while arr_lenth:
     #采用range函数生成一个序列，比列表实际元素少一位数字，因为如果
@@ -13,8 +13,9 @@ while arr_lenth:
     for i in range(arr_lenth-1):
         if arr[i] > arr[i+1]:
             arr[i],arr[i+1] = arr[i+1],arr[i]
+        #打印每次冒泡排序后的结果
+        print(arr)
     #每完成一次冒泡，arr_lenth 减一，当列表中所有元素都完成冒泡循环结束
     arr_lenth-=1        
-
 #打印冒泡完成后的列表
 print(arr)
