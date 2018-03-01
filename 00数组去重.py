158e0beb2c70ea618e46e77fee058b56f0ab5697
xilousong <xilou19@163.com>
2018-03-01 23:02:22 +0800

    change file 00数组去重.py

--- 00数组去重.py
+++ 00数组去重.py
@@ -5,10 +5,7 @@ arr = [1,2,3,4,2,12,3,14,3,14,3,21,2,2,3,4111,22,3333,4]
 arr_new = []
 
 for i in range(len(arr)):
-    if arr[i] in arr_new:
-        continue
-    else:
+    if arr[i] not in arr_new:
         arr_new.append(arr[i])
-    print(arr_new)
 #打印去重后的新的列表
 print(arr_new)
